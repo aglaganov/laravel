e6c80f343a670f0fb2445a9f1cce1aa527533492
aglaganov <aglaganov@gmail.com>
2020-11-04 18:53:18 -0500

    add auth and add Profile Model

--- freeCodeGram/routes/web.php
+++ freeCodeGram/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+
