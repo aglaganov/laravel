ab0ac129582acbad6d7992c4475fbdd4efa1fd7e
aglaganov <aglaganov@gmail.com>
2020-11-05 17:57:05 -0500

    add Profile policy and update post page

--- freeCodeGram/resources/views/posts/show.blade.php
+++ freeCodeGram/resources/views/posts/show.blade.php
@@ -7,8 +7,18 @@
             <img src="/storage/{{ $post->image }}" class="w-100" alt="">
         </div>
         <div class="col-4">
-            <h3>{{ $post->user->username }}</h3>
-            <p>{{ $post->caption }}</p>
+            <div>
+                <div class="d-flex align-items-center">
+                    <div class="pr-3">
+                        <img src="/storage/{{ $post->user->profile->image }}" class="w-100 rounded-circle" style="max-width:40px;" alt="">
+                    </div>
+                   <div>
+                       <div class="font-weight-bold"><a href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a></div>
+                    </div>
+                </div>
+            </div>
+
+            <p><span class="font-weight-bold"><a href="/profile/{{ $post->user->id }}">{{ $post->user->username }}</a></span> {{ $post->caption }}</p>
         </div>
     </div>
 </div>
--- freeCodeGram/resources/views/profiles/index.blade.php
+++ freeCodeGram/resources/views/profiles/index.blade.php
@@ -3,13 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-5"><img src="https://scontent-lga3-2.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-lga3-2.cdninstagram.com&_nc_ohc=jtfqS8Lrv4AAX-SApVN&oh=319045d1879ec32615c9bf34b7dc804d&oe=5FCD4767" class="rounded-circle" alt=""></div>
+        <div class="col-3 p-5"><img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100" alt=""></div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <div><h1>{{ $user->username }}</h1></div>
+                @can('update', $user->profile)
                 <a href="/p/create">Add New Post</a>
+                @endcan
             </div>
+            @can('update', $user->profile)
             <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="col-2"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="col-2"><strong>59,1тыс.</strong> подписчиков</div>
